2660dc8ed4f7c25af97e8e415abaada9c845e03b
Milton Calderon <mvcalderonc@correo.udistrital.edu.co>
2020-12-15 22:13:09 -0500

    feat:se ajusta la plantilla de invitación a los docentes, se ajusta el modelo de notificación para admitir campos html

--- services/correo/correo_enviar.go
+++ services/correo/correo_enviar.go
@@ -45,7 +45,7 @@ func OAuthGmailService() {
 
 	GmailService = srv
 	if GmailService != nil {
-		fmt.Println("Email service is initialized \n")
+		fmt.Println("Email service is initialized")
 	}
 }
 
