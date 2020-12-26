49b10f4911fb5e309d7996c781e08b9e0d449b3f
BrayanB24 <babermudezb@correo.udistrital.edu.co>
2020-12-26 09:19:32 -0500

    devops: modificando entrypoint.sh para incluir variable de autenticación de google con SSM

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 set -u
 set -o pipefail
 
+if [ -n "${PARAMETER_STORE:-}" ]; then
+export GOOGLE_MID_DRIVE_PRIVATE_KEY="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/google_mid/apigoogle/auth --output text --query Parameter.Value)"
+fi
+
 exec ./main "$@"
