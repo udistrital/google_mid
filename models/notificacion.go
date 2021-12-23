a22f4aa098b949492c96a7c0f768312a2bc00f27
JQuinteroC <jolquinteroc@correo.udistrital.edu.co>
2021-12-23 01:39:00 -0500

    feat: ajuste de variable. udistrital/sga_cliente#776

--- models/notificacion.go
+++ models/notificacion.go
@@ -17,7 +17,11 @@ type Notificacion struct {
 type ProduccionAcademica struct{
 	Fecha                   string
 	NombreDocente           string
+	NombreEstudiante		string
 	llaveConsulta           string
+	EspacioAcademico		string
+	FechaInicio				string
+	FechaFin				string
 	ContenidoProduccion     template.HTML
 	UrlCreacionCuentaLogin  template.HTML
 	UrlRechazarEvaluacion   template.HTML
